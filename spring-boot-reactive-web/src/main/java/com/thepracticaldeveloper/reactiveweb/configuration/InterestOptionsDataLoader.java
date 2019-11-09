b44011ef403d3c799e6f61815fd46046475f9ddb
jaglinsub <jaglinsub@gmail.com>
2019-11-09 08:44:16 -0600

    Modified Interest data loader to load only when data is not present in database

--- spring-boot-reactive-web/src/main/java/com/thepracticaldeveloper/reactiveweb/configuration/InterestOptionsDataLoader.java
+++ spring-boot-reactive-web/src/main/java/com/thepracticaldeveloper/reactiveweb/configuration/InterestOptionsDataLoader.java
@@ -4,12 +4,14 @@ import com.thepracticaldeveloper.reactiveweb.domain.AnswerOptions;
 import com.thepracticaldeveloper.reactiveweb.domain.InterestOptions;
 import com.thepracticaldeveloper.reactiveweb.repository.AnswerOptionsMongoReactiveRepository;
 import com.thepracticaldeveloper.reactiveweb.repository.InterestOptionsMongoReactiveRepository;
-import com.thepracticaldeveloper.reactiveweb.repository.UserMongoReactiveRepository;
-import com.thepracticaldeveloper.reactiveweb.repository.UserTypeMongoReactiveRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+
+import java.util.Hashtable;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Component
 public class InterestOptionsDataLoader implements CommandLineRunner {
@@ -24,25 +26,53 @@ public class InterestOptionsDataLoader implements CommandLineRunner {
         this.answerOptionsMongoReactiveRepository = answerOptionsMongoReactiveRepository;
     }
 
-
-    public void run2(final String... args) throws Exception {
+@Override
+    public void run(final String... args) throws Exception {
         try {
-            AnswerOptions answerOptions = new AnswerOptions();
-            answerOptions.setAnswerName("With A Team");
+            if (interestOptionsMongoReactiveRepository.count().block() > 0L) {
+                return;
+            }
+            Hashtable<String, String[]> intTable = new Hashtable<String, String[]>();
+            intTable.put("I’m looking for more opportunities to", new String[]{"Learn Real-World Skills", "Build A Career"});
+            intTable.put("At school, when I’m given a project, I’d rather work", new String[]{"With A Team", "Independently"});
+            intTable.put("My favorite activity to do outside of school is", new String[]{"Play Basketball", "Go To Library"});
+            intTable.put("Ideally, before I graduate I want to know how to", new String[]{"Respect Others", "Listen More and Talk Less"});
+            intTable.put("After I graduate high school, I think I want to be a", new String[]{"Nurse", "Engineer"});
+
+            AtomicInteger displayOrder = new AtomicInteger(1);
+            intTable.forEach( (k, v) -> {
+
+                AnswerOptions answerOptionsArr[] = new AnswerOptions[v.length];
+                for (int i = 0; i < v.length; i++) {
+                    AnswerOptions answerOptions = new AnswerOptions();
+                    answerOptions.setAnswerName(v[i]);
+                    answerOptions = answerOptionsMongoReactiveRepository.save(answerOptions).block();
+                    answerOptionsArr[i] = answerOptions;
+                }
+                InterestOptions interestOptions = new InterestOptions();
+                interestOptions.setInterestQuestionName(k);
+                interestOptions.setInterestQuestion(k);
+                interestOptions.setDisplayOrder(displayOrder.getAndIncrement());
+                interestOptions.setAnswerOptions(answerOptionsArr);
+                interestOptionsMongoReactiveRepository.save(interestOptions).block();
+            });
+
+            /*AnswerOptions answerOptions = new AnswerOptions();
+            answerOptions.setAnswerName("Learn Real-World Skills");
             answerOptionsMongoReactiveRepository.save(answerOptions).block();
 
             AnswerOptions answerOptions1 = new AnswerOptions();
-            answerOptions1.setAnswerName("Independently");
+            answerOptions1.setAnswerName("Build A Career");
             answerOptionsMongoReactiveRepository.save(answerOptions1).block();
 
             AnswerOptions answerOptionsArr[] = {answerOptions, answerOptions1};
 
             InterestOptions interestOptions = new InterestOptions();
-            interestOptions.setInterestQuestionName("Test");
-            interestOptions.setInterestQuestion("What is test");
+            interestOptions.setInterestQuestionName("Looking for");
+            interestOptions.setInterestQuestion("I’m looking for more opportunities to");
             interestOptions.setDisplayOrder(1);
             interestOptions.setAnswerOptions(answerOptionsArr);
-            interestOptionsMongoReactiveRepository.save(interestOptions).block();
+            interestOptionsMongoReactiveRepository.save(interestOptions).block();*/
 
         }
         catch (Exception e) {
@@ -50,8 +80,8 @@ public class InterestOptionsDataLoader implements CommandLineRunner {
         }
     }
 
-    @Override
-    public void run(final String... args) throws Exception {
+    //@Override
+    public void run2(final String... args) throws Exception {
 
     }
 
